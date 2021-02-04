bee4754c3cbc38348d283289154711653f64e8fd
Sanjay <sanjayhmh1969@gmail.com>
2021-02-04 07:35:58 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About This App</h1>
+            <h3>App to Search Github User's build by <i><strong>Sanjay Goswami</strong> </i></h3>
+            <h3>Version :1.0.0</h3>
+            <h3>Build with react <strong>Hooks And Reducer</strong></h3>
+            <a href="#" className='btn btn-success my-1'>More detail's</a>
+
+        </Fragment>
+    )
+}
+
+export default About
